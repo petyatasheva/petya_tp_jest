c42aced4c7811c0bfe727846a2a656a651d982d8
petya tasheva <petya.tasheva@ensiie.fr>
2019-03-28 18:52:53 +0100

    test >= 3000

--- math.js
+++ math.js
@@ -9,13 +9,8 @@ Util.factorial = (n) => {
         throw 'error num not integer'
     }
 
-
-    if (n === 0) {
-        return 1;
-    }
-
     if (n >= 3000) {
-        throw 'n too large'
+        throw 'sorry n>=1000'
     }
 
     if (n < 0) {
